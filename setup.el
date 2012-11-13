3f4f720b572f8c89dbe88c0513864df54dfe9efe
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-11-14 08:36:44 +0900

    * setup.el: batch export setup

---
+++ setup.el
@@ -0,0 +1,18 @@
+(setq org-install-dir "~/.emacs.d/el-get/org-mode")
+(add-to-list 'load-path (concat org-install-dir "/contrib/lisp"))
+(add-to-list 'load-path (concat org-install-dir "/lisp"))
+(require 'org-e-beamer)
+
+(add-to-list 'org-e-latex-classes
+             '("beamer"
+               "\\documentclass[presentation]{beamer}
+\[DEFAULT-PACKAGES]
+\[PACKAGES]
+\[EXTRA]"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+
+(setq org-e-latex-pdf-process
+      '("pdflatex -interaction nonstopmode %b"
+        "pdflatex -interaction nonstopmode %b"))
