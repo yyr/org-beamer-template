0cecf3009ba77ac1bacf2b867fc9b23ef8c7e9ff
Yakkala Yagnesh Raghava <hi@yagnesh.org>
2012-11-14 10:24:41 +0900

    * export.sh: Change the order (thanks to Suvayu)

--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # batch export
 
-emacs -q --batch -l setup.el \
-    -f org-e-beamer-export-to-latex org-beamer-template.org
+emacs -q --batch -l setup.el org-beamer-template.org \
+    -f org-e-beamer-export-to-latex
 
 pdflatex -interaction nonstopmode org-beamer-template.tex
 
